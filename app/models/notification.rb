e25ea8c9d4ea88d179c50ef667dae0f69146c9e0
Martyn Chamberlin <martyn@perfectioncoding.com>
2016-04-22 10:00:03 -0500

    Accomodate sticky posts
    Fix syntax

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,20 +3,31 @@ class Notification < ActiveRecord::Base
 	def generate
 
         require 'open-uri'
-        require 'twilio-ruby' 
+        require 'twilio-ruby'
         output = ''
         link = ''
 
         doc = Nokogiri::HTML(open(RssToSms.config.rss_url))
 
-        client = Twilio::REST::Client.new RssToSms.config.twilio_account_sid, RssToSms.config.twilio_auth_token 
+        client = Twilio::REST::Client.new RssToSms.config.twilio_account_sid, RssToSms.config.twilio_auth_token
         notifications = Array.new;
+        num_checked = 0;
 
         doc.xpath('//entry').each do |item|
             link = item.xpath('link')[1].attr('href')
             title = item.xpath('title')[0].content
 
-            break if Notification.exists?(guid: link) 
+            # Sometimes publishers will rearrange their content and push out a new article that's
+            # underneath a previously published article. When we encounter the most recently sent
+            # item, we want to look a couple underneath that to make sure nothing sticky is going on
+            if Notification.exists?(guid: link)
+                if num_checked < 2
+                    num_checked += 1
+                    next
+                else
+                    break
+                end
+            end
 
             # SMS limit is 140 and subtract additional for white space
             max_title = 140 - link.length - 1
@@ -33,9 +44,9 @@ class Notification < ActiveRecord::Base
 
             # send SMS
             response = client.messages.create({
-                :from => RssToSms.config.from_phone,  
-                :to => RssToSms.config.to_phone,  
-                :body => message
+                :from => RssToSms.config.from_phone,
+                :to => RssToSms.config.to_phone,
+                :body => notification[:message]
             })
             output += notification[:message]
 
